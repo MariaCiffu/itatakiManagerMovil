14e446b9386191615d260c9ce45bc4dbc99bf3ec
mariaCifuentes-INECO <maria.cifuentes@ineco.com>
2025-09-07 21:47:44 +0200

    bug fix player context

--- src/context/PlayerContextMenu.js
+++ src/context/PlayerContextMenu.js
@@ -1,5 +1,13 @@
 // src/components/PlayerContextMenu.js
-import { StyleSheet, Dimensions } from "react-native";
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 import { MODERN_COLORS } from "../constants/modernColors";
 
 const { width: screenWidth, height: screenHeight } = Dimensions.get("window");
